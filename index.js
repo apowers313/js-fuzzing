a14985e3942d2bc081e1b24ee8341be6d9fdae50
Adam Powers <apowers@ato.ms>
2016-12-08 18:56:34 -0800

    restructuring exports for easy structural decomposition

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-module.exports = require ("./lib/fuzz.js");
+var {
+    MandGTypeManager,
+    MandG,
+    Mutator,
+    Generator
+} = require ("./lib/mandg.js");
+var {
+    Cooker,
+    Recipe
+} = require ("./lib/cooker.js");
+var Fuzz = require ("./lib/fuzz.js");
+
+module.exports = {
+    Fuzz: Fuzz,
+    MandGTypeManager: MandGTypeManager,
+    MandG: MandG,
+    Mutator: Mutator,
+    Generator: Generator,
+    Cooker: Cooker,
+    Recipe: Recipe,
+};
